87a73e208dff3225744739972569112fabc9de86
Parvaiz ZIAR <ziarparvaiz@gmail.com>
2022-07-27 17:07:27 +0300

    fix: Notable drops component is finish #8

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,5 @@ import heroSection from "./components/heroSection.js";
 import sliders from "./components/sliders.js";
 import collections from "./components/collections.js";
 import brands from "./components/brands.js";
+
+sliders(3)
